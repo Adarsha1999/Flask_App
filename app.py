3d07c28792a1192763a6f98db4b112d16fbee087
Adarsha Dinda <adarshadinda@gmail.com>
2023-03-17 00:40:39 +0530

    modified the app

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import os
 access_key = os.getenv('access_key')
 secret_access_key = os.getenv("secret_access_key")
 print(access_key,secret_access_key)
-client = boto3.client('dynamodb',aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name="us-east-1")
+dynamodb = boto3.resource('dynamodb',aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name="us-east-1")
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    response = client.get_item(
-    TableName='Candidates',
-    Key={'CandidateName ':{'S':'atin'}}
-    )
-    print(response)
+    table = dynamodb.Table('Candidates')
+    response = table.get_item(Key={'CandidateName': "atin"})
+    item = response['Item']
+    print(item)
+
     return "Hello World"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
